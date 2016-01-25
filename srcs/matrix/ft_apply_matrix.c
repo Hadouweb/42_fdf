458076d01447bcbd47d827b54ee90cac3f286947
hadouweb <kengo14@hotmail.fr>
2016-01-25 17:22:06 +0100

    iso ok

--- srcs/matrix/ft_apply_matrix.c
+++ srcs/matrix/ft_apply_matrix.c
@@ -9,17 +9,17 @@ void	ft_apply_all_matrix(t_scene *scn)
 	m = ft_init_matrix();
 
 	a = ft_scale_matrix(scn->scale);
-	b = ft_rotation_matrix_z(scn->rot);
+	if (scn->projection == 1)
+		scn->rot = ft_get_vector(-1.1, 0.0, 0.80, 1.0);
+
+		b = ft_rotation_matrix_z(scn->rot);
 	m = ft_muli_matrix(b, a);
 
+	b = ft_rotation_matrix_y(scn->rot);
+	m = ft_muli_matrix(b, m);
 	b = ft_rotation_matrix_x(scn->rot);
 	m = ft_muli_matrix(b, m);
 
-	b = ft_rotation_matrix_y(scn->rot);
-	m = ft_muli_matrix(b, m);
-
-	b = ft_rotation_matrix_y(scn->rot);
-	m = ft_muli_matrix(b, m);
 
 	b = ft_translation_matrix(scn->trans);
 	m = ft_muli_matrix(b, m);
